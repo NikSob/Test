1920ab71e9c011e61f1a6a3251b4fce8562bdd2e
nikita <nikita.sobolev.1997@mail.ru>
2016-11-15 00:17:01 +0300

    hi

--- Rep/lesson2.2/Task2.2.1.c
+++ Rep/lesson2.2/Task2.2.1.c
@@ -8,6 +8,6 @@ int main(void)
 	year =  num / 365;
 	month = (num - (365*year))/30;
 	day = num-(365*year)-(30*month);
-	printf("%d дней - это %d года(лет), %d месяцев и %d дней",num, year,month,day);
+	printf("%d дней - это %d года(лет), %d месяцев и %d дней!",num, year,month,day);
 	return 0;
 }
